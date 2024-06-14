ecb8a48942aaa8df8bb6667f4c91f660830b197e
Joeao <jattardowen@gmail.com>
2024-06-14 11:12:04 +0200

    Linted

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import type { JSX } from "react";
-import "./App.css";
-import { ThemeProvider } from "@/components/theme-provider"
 
+import "./App.css";
 import { InputFile } from "@/components/InputFile";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const App = (): JSX.Element => {
 	return (
-		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+		<ThemeProvider
+			defaultTheme={"dark"}
+			storageKey={"vite-ui-theme"}
+		>
 			<InputFile />
 		</ThemeProvider>
 	);
--- src/components/InputFile.tsx
+++ src/components/InputFile.tsx
@@ -1,13 +1,15 @@
+import type { JSX } from "react";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 export function InputFile(): JSX.Element {
   return (
 	<div className={"grid w-full max-w-sm items-center gap-1.5"}>
-		<Label htmlFor={"picture"}>Picture</Label>
+		<Label htmlFor={"csv"}>CSV</Label>
 
 		<Input
-			id={"picture"}
+			id={"csv"}
 			type={"file"}
 		/>
 	</div>
