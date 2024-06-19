d3e95d7bfeb100a3dae990513ff0ac274297869d
Joeao <jattardowen@gmail.com>
2024-06-19 16:12:47 +0200

    Added foreignKeyMapping validation & data cleaning

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,5 +1,7 @@
 import type { Data } from "@/context/Data.context";
+import DatabaseSchema from "@/data/schema.json";
 import type DataRow from "@/interface/DataRow";
+import client from "@/lib/supabase";
 
 export const validateData = (data: Data["rows"], mapping: Data["mapping"]): DataRow[] => {
 	const cleanData: DataRow[] = [];
@@ -32,14 +34,16 @@ export const validateData = (data: Data["rows"], mapping: Data["mapping"]): Data
 		}
 
 		if (canPush) {
-			cleanData.push(row);
+			if (!Object.values(row).every((val) => val !== "" && val !== null)) {
+				cleanData.push(row);
+			}
 		}
 	}
 
 	return cleanData;
 };
 
-export const formatData = (data: DataRow[], mapping: Data["mapping"]): DataRow[] => {
+export const formatPropNames = (data: DataRow[], mapping: Data["mapping"], foreignKeyMapping: Data["foreignKeyMapping"]): DataRow[] => {
 	return data.map((row) => {
 		const formattedRow: Partial<DataRow> = {};
 
@@ -48,8 +52,83 @@ export const formatData = (data: DataRow[], mapping: Data["mapping"]): DataRow[]
 				const matchedMapping = mapping.find((m) => m[1] === key);
 				formattedRow[matchedMapping![0]] = value;
 			}
+
+			if (foreignKeyMapping.find((m) => m[2] === key)) {
+				const matchedMapping = foreignKeyMapping.find((m) => m[2] === key);
+				formattedRow[matchedMapping![2]] = value;
+			}
 		}
 
 		return formattedRow as DataRow;
 	});
+};
+
+export const mapForeignKeys = async (data: DataRow[], mapping: Data["foreignKeyMapping"]): Promise<DataRow[]> => {
+	const tableNames = [...new Set(mapping.map(([_, tableName]) => tableName))];
+	const clonedData = [...data];
+
+	if (!mapping.length) {
+		return Promise.resolve(data);
+	}
+
+	// Do these synchronously to not cripple Supabase
+	for (let i = 0; i < tableNames.length; i++) {
+		const name = tableNames[i];
+		const existingData = [...new Set(data.flatMap((row) => {
+			const mappingIndex = mapping.findIndex(([_, tableName]) => tableName === name);
+
+			return row[mapping[mappingIndex][2]] || [];
+		}))];
+
+		let query = client()
+			.from(name as any)
+			.select(`${[...new Set(mapping.flatMap((val) => (val[1] === name ? [val[0], val[3]] : [])))].join(",")}`);
+
+		// If less than 100 rows - otherwise we might get error & it's slow
+		if (existingData.length < 100) {
+			query = query.in(`${[...new Set(mapping.flatMap((val) => (val[1] === name ? val[3] : [])))]}`, existingData);
+		}
+
+		const response = await query;
+
+		if (response.status < 300 && response.status >= 200 && response.data) {
+			data.forEach((dataRow, dataIndex) => {
+				const keys2 = Object.keys(dataRow);
+
+				keys2.forEach((key) => {
+					const mappedKeysIndexes = mapping.flatMap((val, index) => (val[2] === key ? index : []));
+
+					mappedKeysIndexes.forEach((mappedKeys) => {
+						if (mappedKeys > -1) {
+							response.data.forEach((requestDataEntry) => {
+								if (requestDataEntry[mapping[mappedKeys][3]] === dataRow[key]) {
+									clonedData[dataIndex][mapping[mappedKeys][4]] = requestDataEntry[mapping[mappedKeys][0]];
+								}
+							});
+						}
+					});
+				});
+			});
+		}
+	}
+
+	return clonedData;
+};
+
+export const cleanDataFields = (data: DataRow[], activeSchema: Data["activeSchema"], activeTable: Data["activeTable"]): DataRow[] => {
+	const dataClone = [...data];
+
+	const expectedKeys = Object.keys(DatabaseSchema.properties[activeSchema].properties.Tables.properties[activeTable].properties.Row.properties);
+
+	dataClone.forEach((entry, dataIndex) => {
+		const keys = Object.keys(entry);
+
+		keys.forEach((key) => {
+			if (!expectedKeys.includes(key)) {
+				delete dataClone[dataIndex][key];
+			}
+		});
+	});
+
+	return dataClone;
 };
